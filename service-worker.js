66f4b248ac9ef90d7b3e1439184857eb0abc9b5f
Stan <stvanbaarsen@gmail.com>
2020-02-02 17:07:02 +0100

    improve service worker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,11 @@
-console.log("TEST");
+self.addEventListener('install', (event) => {
+	console.log('Inside the install handler:', event);
+});
+
+self.addEventListener('activate', (event) => {
+	console.log('Inside the activate handler:', event);
+});
+
+self.addEventListener(fetch, (event) => {
+	console.log('Inside the fetch handler:', event);
+});
