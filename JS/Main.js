57562536cc8e8bcf2fa4e12a53be0a9d741130fb
Stan <stvanbaarsen@gmail.com>
2020-02-04 22:29:09 +0100

    fix JS-less pages

--- JS/Main.js
+++ JS/Main.js
@@ -1,3 +1,16 @@
+refreshMath = MathJax.typeset;
+
 window.onload = function () {
-	console.log("testabcdef");
+	if ('serviceWorker' in navigator) {
+		window.addEventListener('load', () => {
+			navigator.serviceWorker.register('../service-worker.js');
+		});
+	}
+
+	var mathEls = document.querySelectorAll("mathEl");
+	for (let i = 0; i < mathEls.length; i++) {
+		let el = mathEls[i];
+		el.innerHTML = "\\(" + el.getAttribute("data") + "\\)";
+	}
+	refreshMath();
 };
