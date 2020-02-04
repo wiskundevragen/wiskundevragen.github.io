e857aede5c0abcad025cb0569d430c1b558cde83
Stan <stvanbaarsen@gmail.com>
2020-02-04 22:34:28 +0100

    better impl

--- JS/Main.js
+++ JS/Main.js
@@ -1,4 +1,12 @@
 refreshMath = MathJax.typeset;
+fixMath = function () {
+	var mathEls = document.querySelectorAll("mathEl");
+	for (let i = 0; i < mathEls.length; i++) {
+		let el = mathEls[i];
+		el.innerHTML = "\\(" + el.getAttribute("data") + "\\)";
+	}
+	refreshMath();
+}
 
 window.onload = function () {
 	if ('serviceWorker' in navigator) {
@@ -7,10 +15,5 @@ window.onload = function () {
 		});
 	}
 
-	var mathEls = document.querySelectorAll("mathEl");
-	for (let i = 0; i < mathEls.length; i++) {
-		let el = mathEls[i];
-		el.innerHTML = "\\(" + el.getAttribute("data") + "\\)";
-	}
-	refreshMath();
+	fixMath();
 };
