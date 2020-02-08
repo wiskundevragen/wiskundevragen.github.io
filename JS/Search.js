eb961d5a69071a057f58e6387aba20e92b502214
Stan <stvanbaarsen@gmail.com>
2020-02-09 00:52:34 +0100

    fix pageload

--- JS/Search.js
+++ JS/Search.js
@@ -1,1 +1,3 @@
-document.getElementById("searchQuery").innerHTML = location.href.split("q=")[1].split("&")[0];
+onload = function () {
+	document.getElementById("searchQuery").innerHTML = location.href.split("q=")[1].split("&")[0];
+}
